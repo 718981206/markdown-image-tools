74290d31e70d26ef2e880b67acab142052de1a15
GitHub Action <action@github.com>
2024-11-01 07:13:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/718981206/markdownimagetools/MyBundle.kt
+++ src/main/kotlin/com/github/718981206/markdownimagetools/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.718981206.markdownimagetools
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/718981206/markdownimagetools/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/718981206/markdownimagetools/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.718981206.markdownimagetools.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
